075d0e3888d201874a35c656919a4c47c37e3c5b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-26 16:29:20 +0200

    Update

--- Email_sender/main.py
+++ Email_sender/main.py
@@ -1,5 +1,5 @@
 from email.message import EmailMessage
+from userdata import user_email, user_password
 
-
-email_sender = "tkwebdev0408@gmail.com"
-
+email_sender = user_email
+email_password = user_password

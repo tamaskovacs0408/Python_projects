26d2f3dd77bb696ebc2cdf6e351d0079ff65e3b1
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-26 16:39:32 +0200

    Update

--- Email_sender/main.py
+++ Email_sender/main.py
@@ -3,3 +3,16 @@ from userdata import user_email, user_password
 
 email_sender = user_email
 email_password = user_password
+email_reciever = ""
+
+subject = "Python email"
+body = """
+This email is sent by a Python code.
+"""
+
+em = EmailMessage()
+em["From"] = email_sender
+em["To"] = email_reciever
+em["Subject"] = subject
+em.set_content(body)
+
